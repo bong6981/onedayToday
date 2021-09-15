52a5175a0cd2113eb5f5ae2bbab8a2af6468508a
bong6981 <kbk5190@naver.com>
2021-09-15 09:42:43 +0900

    Init project

---
+++ be/onedayToday/src/main/java/bong/onedayToday/OnedayTodayApplication.java
@@ -0,0 +1,13 @@
+package bong.onedayToday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedayTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnedayTodayApplication.class, args);
+	}
+
+}
